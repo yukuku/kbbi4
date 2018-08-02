bf7ab21b5c0fcffa37b084255270718f08b86b22
Yuku on yuku8 <yukuku@gmail.com>
2018-08-02 15:10:24 +0800

    Add backrujuks to show nonbaku acus. Annotate python variables.

--- android/app/src/main/java/yuku/kbbi/main/UnknownPage.java
+++ android/app/src/main/java/yuku/kbbi/main/UnknownPage.java
@@ -1,6 +1,7 @@
 package yuku.kbbi.main;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,7 +11,7 @@ import yuku.kbbi.R;
 public class UnknownPage extends ContentPage {
 	@Nullable
 	@Override
-	public View onCreateView(final LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable final Bundle savedInstanceState) {
+	public View onCreateView(@NonNull final LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable final Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.page_main_unknown, container, false);
 	}
 }
